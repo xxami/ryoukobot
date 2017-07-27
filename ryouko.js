31573d753ad54fb1e509e40460125d02178de6a3
lilah <lilah.willacott@gmail.com>
2017-07-27 18:29:27 +0100

    add basic working commands

--- ryouko.js
+++ ryouko.js
@@ -1,12 +1,23 @@
 
 const dotenv = require('dotenv');
-const twitchbot = require('node-twitchbot');
+const TwitchBot = require('node-twitchbot');
 
 dotenv.config({path: 'ryouko.env'});
 
-const bot = new twitchbot({
+const bot = new TwitchBot({
 	username: process.env.TWITCH_USERNAME,
 	oauth: process.env.TWITCH_OAUTH,
 	channel: process.env.TWITCH_CHANNEL
 });
 
+bot.connect().then(() => {
+
+	bot.listenFor('#help', (err, chatter) => {
+		bot.msg('this is the help message');
+	});
+
+	bot.listenFor('#hugme', (err, chatter) => {
+		bot.msg('/me hugs ' + chatter.user);
+	});
+
+});
